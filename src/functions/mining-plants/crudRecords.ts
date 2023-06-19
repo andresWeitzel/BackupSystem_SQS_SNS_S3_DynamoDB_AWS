02dfcdced5b37f1af8f36701e7cf5609bd7c808a
Andrés Weitzel <andres96energy@hotmail.com>
2023-06-18 23:42:18 -0300

    Checked insert lambda

--- src/functions/mining-plants/crudRecords.ts
+++ src/functions/mining-plants/crudRecords.ts
@@ -1,3 +1,5 @@
+//Interfaces
+import { MiningPlants } from "src/interfaces/MiningPlants";
 //Enums
 import { statusCode } from "src/enums/http/statusCode";
 import { value } from "src/enums/general/values";
@@ -5,15 +7,13 @@ import { value } from "src/enums/general/values";
 import { requestResult } from "src/helpers/http/bodyResponse";
 import { validateHeadersAndKeys } from "src/helpers/validations/headers/validateHeadersAndKeys";
 import { formatToJson } from "src/helpers/format/formatToJson";
-import { generateUuidV4 } from "src/helpers/math/generateUuid";
-import { validateObject } from "src/helpers/validations/models/validateObject";
-import { formatToBigint } from "src/helpers/format/formatToNumber";
 import { insertItems } from "src/helpers/dynamodb/operations/insertItems";
-
-
 //Const/Vars
 let eventHeaders: any;
 let checkEventHeadersAndKeys: any;
+let item: MiningPlants;
+let itemTransactionResult:any;
+let itemsList: Array<MiningPlants>;
 let msg: string;
 let code: number;
 const MINING_PLANTS_TABLE_NAME = process.env.DYNAMO_MINING_PLANTS_TABLE_NAME;
@@ -27,6 +27,9 @@ const MINING_PLANTS_TABLE_NAME = process.env.DYNAMO_MINING_PLANTS_TABLE_NAME;
 module.exports.insert = async (event: any) => {
     try {
         //Init
+        item = null;
+        itemTransactionResult = null;
+        itemsList = [];
 
         //-- start with validation headers and keys  ---
         eventHeaders = await event.headers;
@@ -38,34 +41,37 @@ module.exports.insert = async (event: any) => {
         }
         //-- end with validation headers and keys  ---
 
-
+        //-- start with db transaction  ---
         let eventBody = await formatToJson(event.body);
-        let item;
-        let ss;
-
 
         for (let i of eventBody) {
 
             item = {
-                uuid: i.geom,
-                nombre: i.nombre,
-                empresa: i.empresa,
-                tipo_yacimiento: (i.tipo_yacim ==null) ? "-" : i.tipo_yacim,
-                estado_vigente: i.estado,
-                mineral_principal: i.mineral_pr,
-                geolocalizacion: i.geojson
+                uuid: (i.geom == null || i.geom == '') ? "-" : i.geom,
+                nombre: (i.nombre == null || i.nombre == '') ? "-" : i.nombre,
+                empresa: (i.empresa == null || i.empresa == '') ? "-" : i.empresa,
+                tipo_yacimiento: (i.tipo_yacim == null || i.tipo_yacim == '') ? "-" : i.tipo_yacim,
+                estado_vigente: (i.estado == null || i.estado == '') ? "-" : i.estado,
+                mineral_principal: (i.mineral_pr == null || i.mineral_pr == '') ? "-" : i.mineral_pr,
+                geolocalizacion: (i.geojson == null || i.geojson == '') ? "-" : i.geojson
             }
-            ss = await insertItems(MINING_PLANTS_TABLE_NAME, item);
+            itemTransactionResult = await insertItems(MINING_PLANTS_TABLE_NAME, item);
+
+            if (itemTransactionResult == undefined || itemTransactionResult == null) return;
+
+            itemsList.push(item);
         }
 
 
-        if (ss != null) {
-            return await requestResult(statusCode.OK, ss);
-        } else {
-            return await requestResult(statusCode.BAD_REQUEST, 'Bad Request');
+        if (itemTransactionResult == value.IS_NULL || itemTransactionResult == value.IS_UNDEFINED || !(itemTransactionResult.length)) {
+            return await requestResult(
+                statusCode.INTERNAL_SERVER_ERROR,
+                "An error has occurred, the object has not been inserted into the database. Try again"
+            );
         }
 
-
+        return await requestResult(statusCode.OK, itemsList);
+        //-- end with db transaction  ---
 
     } catch (error) {
         code = statusCode.INTERNAL_SERVER_ERROR;
---
+++ src/interfaces/MiningPlants.ts
@@ -0,0 +1,9 @@
+export interface MiningPlants{
+    uuid: string,
+    nombre: string,
+    empresa: string,
+    tipo_yacimiento: string,
+    estado_vigente: string,
+    mineral_principal: string,
+    geolocalizacion: string
+}
